847cef70e93b9e056caf09acef7e30d026b657bb
jmmascorro <jessemascorro88@gmail.com>
2022-08-10 16:38:59 -0500

    changed some styling in scss to finalize the last touches to design and changed the title of the game to something more simple

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-//Each square should output one letter input from clicked buttons
-//when enter button is clicked JS should check wordToGuess against guessedWord
-//if letter is in the word and in the correct place, the square should turn green
-//if letter is in the word but in wrong place, the square should turn yellow
-//if letter is not in the word, the square should turn grey
-//once word is guessed next TR should activate for next word to guess
-//if word is guessed the game stops and should have a button to restart game, and generate new word to guess
-// input buttons should turn color accoringly to guessed letters
-
-//when letter is clicked each innertext of each button should get saved in a variable.
-//Each square should output one letter input from clicked buttons.
-// should be able to click on button and output a seperate letter for each square
-// create var to store several words to guess from
-// generate a random word from guessingWords array to use for guessing
-// check to see if randomWord and created guessed word match by using enter button
-// check if letters match between guessed word and random word
-//if all letters match then squares turn green
-
 const lettersBtn = document.querySelectorAll(".letter");
 const squaresOfRowOne = document.querySelectorAll(".row_one");
 const squaresOfRowTwo = document.querySelectorAll(".row_two");
